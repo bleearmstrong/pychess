4354a3b9ef080d66f12be6740dfce564852b0917
goodtimeslim <bleearmstrong@gmail.com>
2018-08-19 14:34:22 -0700

    add Bishop

--- main.py
+++ main.py
@@ -38,14 +38,13 @@ class Pawn(Piece):
         possible_positions = [(self.location[0] - (1 * self.multiplier), self.location[1] - 1)
                               , (self.location[0] - (1 * self.multiplier), self.location[1])
                               , (self.location[0] - (1 * self.multiplier), self.location[1] + 1)]
-        valid_moves_0 = list()
+        valid_moves_0 = []
         for position in possible_positions:
             if not (position[0] < 0 or position[0] > 7 or position[1] < 0 or position[1] > 7):
                 valid_moves_0.append(position)
-        captures = list()
         locations = my_board.positions.copy()
         relevant_squares = {key: locations[key] for key in valid_moves_0}
-        valid_moves_1 = list()
+        valid_moves_1 = []
         for square in relevant_squares:
             if not relevant_squares[square] or relevant_squares[square].color != self.color:
                 valid_moves_1.append(square)
@@ -62,7 +61,7 @@ class Rook(Piece):
     def get_valid_moves(self):
         # TODO add castling
         locations = my_board.positions.copy()
-        valid_moves = list()
+        valid_moves = []
         for ii in [1, -1]:
             i = 1
             while 0 <= self.location[0] + i*ii < 8:
@@ -90,8 +89,36 @@ class Rook(Piece):
                     valid_moves.append((self.location[0], new_y))
                     i += 1
                     break
-                if locations[(new_x, self.location[1])].color == self.color:
+                if locations[(self.location[0], new_y)].color == self.color:
                     break
+        return valid_moves
+
+
+class Bishop(Piece):
+
+    def __init__(self, color, location):
+        Piece.__init__(self, 'bishop', color, location)
+
+    def get_valid_moves(self):
+        valid_moves = []
+        locations = my_board.positions
+        for ii in [1, -1]:
+            for jj in [1, -1]:
+                i = 1
+                new_x = self.location[0] + i*ii
+                new_y = self.location[1] + i*jj
+                while 0 <= new_x < 8 and 0 <= new_y < 8:
+                    if not locations[(new_x, new_y)]:
+                        valid_moves.append((new_x, new_y))
+                        i += 1
+                        new_x = self.location[0] + i * ii
+                        new_y = self.location[1] + i * jj
+                        continue
+                    if locations[(new_x, new_y)].color != self.color:
+                        valid_moves.append((new_x, new_y))
+                        break
+                    if locations[(new_x, new_y)].color == self.color:
+                        break
         return valid_moves
 
 
@@ -129,5 +156,9 @@ my_board.print()
 
 my_board.board[1][1].get_valid_moves()
 
-my_board.board[4][1] = Rook('white', (4, 1))
+# my_board.board[4][1] = Rook('white', (4, 1))
+# my_board.board[4][1].get_valid_moves()
+
+my_board.board[4][1] = Bishop('white', (4, 1))
+my_board.print()
 my_board.board[4][1].get_valid_moves()
